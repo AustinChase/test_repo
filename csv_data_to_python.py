01898cdb309630deef2a8c7c0b41c19789b587a8
ckcole <cole.ckc@gmail.com>
2017-08-19 22:49:36 -0700

    Fix broken function structure and typo

--- csv_data_to_python.py
+++ csv_data_to_python.py
@@ -12,36 +12,6 @@
 # now we need to import the one requirement for this is
 import csv
 
-# lets initialize a list of lists and a list of dictionaries to give you two options to play with the data with
-lsf_list_of_lists = []
-lsf_list_of_dicts = []
-
-# lets start with the list
-# the "with" statement is just pythons weird way of opening files or connections to close them later.
-with open('source_data_v2.csv') as csvfile:
-    # "csvfile" is the variable name we are given this open file
-    reader = csv.reader(csvfile)
-    for row in reader:
-        lsf_list_of_lists.append(row)
-
-    # each row is a list of values.  the lsf_list is a list of these lists.  (like a meta list)
-    # if you look at the csv, you could see the first TWO rows are headers for the column.
-    # do with that what you will.  You could grab each one by saying lsf_list[0] lsf_list[1]
-
-    # now lets do it with as a dictionary.  our file is still open because we are in the with loop.
-    # an optional argument you can give is fieldnames=[<list of words you want for keys>]
-    dict_reader = csv.DictReader(csvfile)
-    for row in dict_reader:
-        lsf_list_of_dicts.append(row)
-
-    # this will give you a list of dictionaries
-    # {'name': 'austin', 'age': 5}
-
-
-# either of these ways works. the second one might be easier to work  with since you can do things like:
-# row['company_name'] == 'Farella Braun'  # True
-
-
 # One extra thing we never went over is getting you this list into a variable.
 # Im creating this function so that you can pull this list out without having to copy and paste this logic.
 # to do this,  make sure you have this script in your working directory, the do the following
@@ -49,5 +19,34 @@ with open('source_data_v2.csv') as csvfile:
 # from csv_data_to_python import gimme_data
 # my_data = gimme_data()
 
-def gimmme_data():
+
+def gimme_data():
+    # lets initialize a list of lists and a list of dictionaries to give you two options to play with the data with
+    lsf_list_of_lists = []
+    lsf_list_of_dicts = []
+
+    # lets start with the list
+    # the "with" statement is just pythons weird way of opening files or connections to close them later.
+    with open('source_data_v2.csv') as csvfile:
+        # "csvfile" is the variable name we are given this open file
+        reader = csv.reader(csvfile)
+        for row in reader:
+            lsf_list_of_lists.append(row)
+
+        # each row is a list of values.  the lsf_list is a list of these lists.  (like a meta list)
+        # if you look at the csv, you could see the first TWO rows are headers for the column.
+        # do with that what you will.  You could grab each one by saying lsf_list[0] lsf_list[1]
+
+        # now lets do it with as a dictionary.  our file is still open because we are in the with loop.
+        # an optional argument you can give is fieldnames=[<list of words you want for keys>]
+        dict_reader = csv.DictReader(csvfile)
+        for row in dict_reader:
+            lsf_list_of_dicts.append(row)
+
+        # this will give you a list of dictionaries
+        # {'name': 'austin', 'age': 5}
+
+    # either of these ways works. the second one might be easier to work  with since you can do things like:
+    # row['company_name'] == 'Farella Braun'  # True
+
     return lsf_list_of_dicts
