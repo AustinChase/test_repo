e78fd94cc0c58da2c8eee83782cfd9905e941584
Austin Chase <austinchase@MacBook.local>
2017-10-06 14:14:32 -0700

    added functions to pull enrollment for all product lines, added function to calculate average enrollments by product

--- code/parse.py
+++ code/parse.py
@@ -25,7 +25,6 @@ if __name__ == '__main__':
     print "there are {} columns in my file".format(len(grabbed_data[0].keys()))
     print "I was called as a script!!"
 
-
 def get_copay():
 
 # call the parse data function
@@ -36,7 +35,7 @@ def get_copay():
     for items in my_data:
         non_kaiser_Rx.append(items['Q121_3_1'])
         non_kaiser_Rx = filter(None, non_kaiser_Rx)
- #remove first descriptor variable since it breaks the int function   
+ #remove first descriptor variable  
     non_kaiser_Rx.remove(non_kaiser_Rx[0])
     
     sum = 0
@@ -52,23 +51,109 @@ def get_copay():
     return average, " 1 "
 
 if __name__ == '__main__':
-    calced_data = get_non_kaiser_copay()
+    calced_data = get_copay()
     print  "The average non Kaiser HMO Rx copay is $%s" %calced_data
 
-def get_enrollment():
+def get_enrollment_HMO_non_kaiser():
     my_data = parse_csv()
-    enrollment = []
-# populate empty list with raw enrollment data
+    enrollment_non_kaiser = []
+# populate empty list with raw enrollment data for Non Kaiser HMO
     for items in my_data:
-        enrollment.append(items['Q63_1'])
+        enrollment_non_kaiser.append(items['Q63_1'])
  # remove the first row in the list, the questions' description       
-    enrollment.remove(enrollment[0])
+    enrollment_non_kaiser.remove(enrollment_non_kaiser[0])
 # use list comprehension to replace None types and/or string blanks with 0s
-    enrollment = [0 if v is '' or None else v for v in enrollment]
-    return enrollment
+    enrollment_non_kaiser = [0 if v is '' or None else v for v in enrollment_non_kaiser]
+    return enrollment_non_kaiser
+
+def get_enrollment_HMO_kaiser():
+# same code as get_enrollment_HMO_non_kaiser, just adding HMO_kaiser     
+    my_data = parse_csv()
+    enrollment_kaiser = []
+    # populate empty list with raw enrollment data for Kaiser HMO
+    for items in my_data:
+        enrollment_kaiser.append(items['Q63_2'])
+
+    enrollment_kaiser.remove(enrollment_kaiser[0])
+
+    enrollment_kaiser = [0 if v is '' or None else v for v in enrollment_kaiser]    
+    
+    return enrollment_kaiser
+
+def get_enrollment_PPO():
+ # same code as above, just adding PPO   
+    my_data = parse_csv()
+    enrollment_PPO = []
+
+    for items in my_data:
+        enrollment_PPO.append(items['Q63_3'])
+
+    enrollment_PPO.remove(enrollment_PPO[0]) 
+
+    enrollment_PPO = [0 if v is '' or None else v for v in enrollment_PPO]
+
+    return enrollment_PPO
+
+def get_enrollment_HDHP():
+ # same code as above, just adding PPO   
+    my_data = parse_csv()
+    enrollment_HDHP = []
+
+    for items in my_data:
+        enrollment_HDHP.append(items['Q63_3'])
+
+    enrollment_HDHP.remove(enrollment_HDHP[0]) 
+
+    enrollment_HDHP = [0 if v is '' or None else v for v in enrollment_HDHP]
+
+    return enrollment_HDHP
+
+def get_enrollment_waiver():
+ # same code as above, just adding PPO   
+    my_data = parse_csv()
+    enrollment_waiver = []
+
+    for items in my_data:
+        enrollment_waiver.append(items['Q63_3'])
+
+    enrollment_waiver.remove(enrollment_waiver[0]) 
+
+    enrollment_waiver = [0 if v is '' or None else v for v in enrollment_waiver]
+
+    return enrollment_waiver
+
+def get_average_enrollment(eHMO, eKHMO, ePPO, eHDHP, eWaiver):
+    my_list = [eHMO,eKHMO, ePPO, eHDHP, eWaiver]
+    enrollment_sum = 0
+    enrollment_sums = [0,0,0,0,0]
+    average_enrollment = [0,0,0,0,0]
+    i = 0
+    a = 0
+    counter = 0
+    # loop through lists of enrollments for each product type
+    for e_list in my_list:
+        # loop through each value of each list
+        for item in e_list:
+            # convert string types to floats and sum values by product
+            enrollment_sum = float(item) + enrollment_sum
+            # create new list for sums
+            enrollment_sums[i] = enrollment_sum
+        # increment counter for outside loop    
+        i += 1
+        
+    # loop through new list of sums
+    for subtotal in enrollment_sums:
+        # calculate lench of each original list
+        counter = len(my_list[a])
+        # populate new list with average enrollemnts
+        average_enrollment[a] = enrollment_sums[a] / counter
+        #increment loop
+        a += 1
+    # return list of averages    
+    return average_enrollment
 
 if __name__ == 'main':
-    calced_enrollment = get_enrollment()
+    calced_enrollment = get_enrollment_HMO_non_kaiser()
     print "Work in progress, returning a scrubbed enrollment list: %s" %calced_enrollment   
 # this function takes a single argument and writes the argument to a new csv file
 def write_data(answers):
